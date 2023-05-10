1989bd0ca117d27c72f740ef1371ddf0ab44bee8
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-05-10 23:41:04 +1000

    Added capacitor for deployment to android

---
+++ android/app/src/main/java/com/danoaky/spacetraders/MainActivity.java
@@ -0,0 +1,5 @@
+package com.danoaky.spacetraders;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,24 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+// TODO: find a better way to turn live reloading on/off via cli or env var:
+// /** If true then live reloading is turned on. */
+// const LIVE_RELOAD = process.argv.includes('-lr') || process.argv.includes('--live-reloading');
+
+// const server: CapacitorConfig['server'] = { 
+//   url: 'http://10.0.2.2:5173',
+//   cleartext: true,
+// };
+const server: CapacitorConfig['server'] = undefined;
+
+const config: CapacitorConfig = {
+  appId: 'com.danoaky.spacetraders',
+  appName: 'Space Traders',
+  webDir: 'build',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+if (server) console.log(`🔴🔥 Running capactitor with live-reload at "${server?.url}"`);
+
+export default config;
