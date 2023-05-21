0844d1f22d604cc7dcceba5bae21cbfe4089aebc
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-05-22 09:24:36 +1000

    Added some more misc utils

--- src/lib/utils/misc.ts
+++ src/lib/utils/misc.ts
@@ -1,4 +1,5 @@
 import type { DataObject, UnwrapData } from '$types';
+import { toastStore, type ToastSettings } from '@skeletonlabs/skeleton';
 
 export function sleep(ms: number): Promise<number> {
 	return new Promise((resolve) => setTimeout(() => resolve(ms), ms));
@@ -42,4 +43,56 @@ export function fastHash(str: string, seed = 0): number {
 	h1 ^= h2 >>> 16;
 	h2 ^= h1 >>> 16;
 	return 2097152 * (h2 >>> 0) + (h1 >>> 11);
+}
+
+const MS_MAP = {
+	ms: 1,
+	millisecond: 1,
+	milliseconds: 1,
+	s: 1000,
+	second: 1000,
+	seconds: 1000,
+	m: 1000 * 60,
+	minute: 1000 * 60,
+	minutes: 1000 * 60,
+	h: 1000 * 60 * 60,
+	hour: 1000 * 60 * 60,
+	hours: 1000 * 60 * 60,
+	d: 1000 * 60 * 60 * 24,
+	day: 1000 * 60 * 60 * 24,
+	days: 1000 * 60 * 60 * 24
+};
+
+/** Converts `number` to milliseconds using a known conversion from `unit`. */
+export function toMS(number: number, unit: keyof typeof MS_MAP) {
+	return number * MS_MAP[unit];
+}
+
+// export function awaitParams<T extends (...args: Promise<any>[]) => any[]>(fn: T) {
+// 	return async (...args: Parameters<T>) => {
+// 		const awaitedArgs = [...await Promise.all(args)];
+// 		return (...awaitedArgs: Parameters<T>) => fn(...awaitedArgs);
+// 		// fn(...awaitedArgs)
+// 	};
+// }
+
+/** Handles errors by showing it on a toast and returning a result. */
+export function handleError<TErrorResult>(
+	cb: (err: Error) => {
+		/** The result to return in case of an error. */
+		result: TErrorResult;
+		/** If true, then don't display toast. */
+		hideToast?: boolean;
+	} & Partial<ToastSettings>
+) {
+	return (err: Error) => {
+		const options = cb(err);
+		if (!options?.hideToast)
+			toastStore.trigger({
+				message: err.message,
+				background: 'bg-red-500',
+				...(options ?? {})
+			});
+		return options.result;
+	};
 }
