de309a1397d511081b7e4ef0f8715e714f258e4f
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-05-22 09:26:14 +1000

    Remove currentToken if applicable

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,8 +1,15 @@
 import { SpaceTradersAPI, SAVED_AGENTS_PATH, entityStore, type SavedAgent, fileStore } from '$lib';
 import { localStorageStore } from '@skeletonlabs/skeleton';
+import { get } from 'svelte/store';
 
 export const savedAgents = entityStore(fileStore<SavedAgent[]>(SAVED_AGENTS_PATH, []));
 
 export const currentToken = localStorageStore('currentAgentToken', '');
+savedAgents.subscribe(async ($savedAgents) => {
+	const savedAgents = await $savedAgents;
+	const token = get(currentToken);
+	if (!savedAgents.length || !savedAgents.find((agent) => agent.token === token))
+		currentToken.set('');
+})();
 
 export const api = new SpaceTradersAPI(currentToken);
