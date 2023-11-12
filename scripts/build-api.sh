33f4884bd139204835581d81d6d6df9f28bd5a31
Daniel Oakman <42539848+danieloakman@users.noreply.github.com>
2023-11-13 10:11:15 +1100

    Added openapi-generator-cli package and update build-api script

--- scripts/build-api.sh
+++ scripts/build-api.sh
@@ -2,17 +2,14 @@
 
 git clone git@github.com:SpaceTradersAPI/api-docs.git
 
-openapi-generator generate \
-  -i api-docs/reference/SpaceTraders.json \
-  -o spacetraders-sdk \
-  -g typescript-axios \
-  --additional-properties=npmName="spacetraders-sdk" \
-  --additional-properties=npmVersion="2.0.0" \
-  --additional-properties=supportsES6=true \
-  --additional-properties=withSeparateModelsAndApi=true \
-  --additional-properties=modelPackage="models" \
-  --additional-properties=apiPackage="api" \
-  --additional-properties=useSingleRequestParameter=true
+# Check if java is installed:
+if ! command -v java &> /dev/null
+then
+  echo "Java could not be found. Please install Java 11 or higher."
+  exit
+fi
+
+pnpm exec openapi-generator-cli generate -i api-docs/reference/SpaceTraders.json -o spacetraders-sdk -g typescript-axios --additional-properties=npmName="spacetraders-sdk" --additional-properties=npmVersion="2.0.0" --additional-properties=supportsES6=true --additional-properties=withSeparateModelsAndApi=true --additional-properties=modelPackage="models" --additional-properties=apiPackage="api"
 
 cd spacetraders-sdk
 pnpm update axios@latest
@@ -22,6 +19,6 @@ mv spacetraders-sdk-2.0.0.tgz ../packages/spacetraders-sdk-2.0.0.tgz
 cd ..
 pnpm add ./packages/spacetraders-sdk-2.0.0.tgz
 
-# Clean up:
+# # Clean up:
 rm -rf api-docs
 rm -rf spacetraders-sdk
